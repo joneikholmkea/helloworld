2350e81180be0a80284e57ab80941cb3e3b074f5
Jon Eikholm <jone@kea.dk>
2021-10-29 15:13:12 +0200

    java 17 .

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -1,10 +1,7 @@
 package com.example.helloworld;
 
-import com.mysql.cj.jdbc.JdbcConnection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.sql.Connection;
 
 @Controller
 public class HomeController {
